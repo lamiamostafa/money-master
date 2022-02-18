174691bfd34b0b82644ed45720c60d547eb39e4b
lamiamostafa <lamiamostafa791@gmail.com>
2022-02-18 22:34:34 +0600

    assignment checked

--- js/app.js
+++ js/app.js
@@ -2,24 +2,29 @@ function getInputValue(product) {
     const productInput = document.getElementById(product + '-cost');
     const productNumber = parseInt(productInput.value);
 
+
     if (isNaN(productNumber) || productNumber < 0 || productNumber == '') {
         productInput.value = '';
         return alert("Please give a positive number");
     }
 
-    productInput.value = '';
+    // productInput.value = '';
     return productNumber;
 }
+
 
 function calculateTotal() {
     let costOfFood = getInputValue('food');
     let costOfRent = getInputValue('rent');
     let costOfCloth = getInputValue('cloth');
     let totalIncome = getInputValue('income');
+
     let totalExpenses = costOfFood + costOfRent + costOfCloth;
+
     if (totalExpenses > totalIncome) {
-        return alert('Income must be largr than total expenses');
+        return alert('Income must be larger than total expenses');
     }
+
     let balance = totalIncome - totalExpenses;
 
     //update on the html
@@ -27,17 +32,21 @@ function calculateTotal() {
     document.getElementById('balance').innerText = balance;
     return balance;
 
+
 }
 
 function updateBalance() {
+    let totalIncome = getInputValue('income');
     let savingInput = getInputValue('save');
-    let newTotalIncome = totalIncome.innerText;
-    let newBalance = balance.innerText;
-    let save = (newTotalIncome * (savingInput / 100));
+
+    let newBalance = parseInt(balance.innerText);
+    let save = (totalIncome * (savingInput / 100));
     if (save > newBalance) {
         return alert('Not have enough  balance to save')
     }
+    console.log(balance, save);
     let RemainingBalance = newBalance - save;
+
 
     //update on the html
     document.getElementById('save-balance').innerText = save;
